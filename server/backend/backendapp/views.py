faebb140c61a1618554956078d3533a94c1bffbe
ChoukraneYasmine <125039629+ChoukraneYasmine@users.noreply.github.com>
2024-03-02 00:18:26 +0100

    Update views.py

--- server/backend/backendapp/views.py
+++ server/backend/backendapp/views.py
@@ -1,32 +1,30 @@
-from django.http import JsonResponse
-from django.db.models import Count, Q
-from .models import Utilisateurs, EvaluationArticle, Checkpoint, Investment, Podcasts, Articles, Company, SectorCompany
+from django.shortcuts import render
+from django.shortcuts import render
+from rest_framework import generics
+from .models import Company, Investment, Type, Sectors, SectorCompany
+from .serializers import CompanySerializer, InvestmentSerializer, TypeSerializer, SectorSerializer, SectorCompanySerializer
 
-def get_ordered_companies(request):
-    user_id = request.GET.get('user_id')
+class CompanyRetrieve(generics.RetrieveAPIView):
+    queryset = Company.objects.all()
+    serializer_class = CompanySerializer
 
-    # Retrieve liked articles by the user
-    liked_articles = EvaluationArticle.objects.filter(user_id=user_id, rating__gte=4).values_list('article_id', flat=True)
+class InvestmentRetrieve(generics.RetrieveAPIView):
+    queryset = Investment.objects.all()
+    serializer_class = InvestmentSerializer
+class InvestmentList(generics.ListAPIView):
+    queryset = Investment.objects.all()
+    serializer_class = InvestmentSerializer
+class TypeRetrieve(generics.RetrieveAPIView):
+    queryset = Type.objects.all()
+    serializer_class = TypeSerializer
 
-    # Retrieve completed courses by the user
-    completed_courses = Checkpoint.objects.filter(user_id=user_id, is_done=True).values_list('lesson__section__course_id', flat=True)
+class SectorRetrieve(generics.RetrieveAPIView):
+    queryset = Sectors.objects.all()
+    serializer_class = SectorSerializer
+class CompanySectorsList(generics.ListAPIView):
+    serializer_class = SectorSerializer
 
-   
-    # Retrieve sectors associated with liked articles
-    sectors = SectorCompany.objects.filter(company_id__in=liked_articles).values_list('name_sector__name_sector', flat=True)
-
-    # Order companies based on sectors and types associated with user's interactions
-    ordered_companies = Company.objects.annotate(
-        num_liked_articles=Count('sectorcompany__name_sector', filter=Q(sectorcompany__name_sector__in=sectors)),
-        num_completed_courses=Count('sectorcompany__name_sector', filter=Q(sectorcompany__name_sector__in=completed_courses)),
-          ).order_by('-num_liked_articles', '-num_completed_courses').distinct()
-
-    # Serialize the ordered companies into JSON
-    serialized_companies = [{
-        'company_name': company.company_name,
-        'num_liked_articles': company.num_liked_articles,
-        'num_completed_courses': company.num_completed_courses,
-        
-    } for company in ordered_companies]
-
-    return JsonResponse({'companies': serialized_companies})
+    def get_queryset(self):
+        company_id = self.kwargs['company_id']
+        return Sectors.objects.filter(sectorcompany__company_id=company_id)
+# Create your views here.
