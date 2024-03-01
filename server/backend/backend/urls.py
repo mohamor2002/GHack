ed8abb8633e1a53402d96cacba473ddc82aba95a
Khadir Amina. <91703189+minaiscoding@users.noreply.github.com>
2024-03-01 22:28:58 +0100

    working invest show

--- server/backend/backend/urls.py
+++ server/backend/backend/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from backendapp import views
 
 urlpatterns = [
-    #path('companies/', views.get_companies, name='get_companies'),
+    path('companies/', views.get_ordered_companies, name='get_ordered_companies'),
     # Add other URL patterns as needed
 ]
