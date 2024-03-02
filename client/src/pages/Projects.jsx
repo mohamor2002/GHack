ad82af94d537ba4a8f59eaa962089b7c9bf9bd51
A-Kireche <la_kireche@esi.dz>
2024-03-02 03:57:45 +0100

    practice

--- client/src/pages/Projects.jsx
+++ client/src/pages/Projects.jsx
@@ -1,23 +1,25 @@
 import React from "react";
 import ProjectCard from "../components/ProjectCard";
 import Navbar from "../components/Navbar";
+import PracticeCard from "../components/PracticeCard";
 const Projects = () => {
    const Cards = [
     {
-      Title: "No Cash Flow !",
-      Difficulty: "Easy",
+      title: "No Cash Flow !",
+      difficulty: "Easy",
       keywords: ["Python", "Finance"],
     },
     {
-      Title: "No Cash Flow !",
-      Difficulty: "Easy",
+      title: "No Cash Flow !",
+      difficulty: "Easy",
       keywords: ["Python", "Finance"],
     },
     {
-      Title: "No Cash Flow !",
-      Difficulty: "Easy",
+      title: "No Cash Flow !",
+      difficulty: "Easy",
       keywords: ["Python", "Finance"],
     },
+    
   ];
   return (
     <div>
@@ -25,11 +27,10 @@ const Projects = () => {
       <div class="grid grid-cols-3 gap-4 p-10 bg-main-gray">
         {Cards.map((card, index) => (
           <ProjectCard 
-            title={card.Title}
-            difficulty={card.Difficulty}
-            keywords={card.keywords}
+            card={card}
           />
         ))}
+
       </div>
     </div>
   );
